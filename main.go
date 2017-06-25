7f497aa012182325f10db916ee25254342668e98
Rahul Ghose <rahul.g@browserstack.com>
2017-06-25 21:50:52 +0530

    multi part file joining

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 	"sync"
 )
 
 var wg sync.WaitGroup
 
-func getFileInfo(fileName string) (int,string) {
+func getFileInfo(fileName string) (int, string) {
 	res, err := http.Head(fileName)
 	if err != nil {
 		panic(err)
@@ -20,14 +22,19 @@ func getFileInfo(fileName string) (int,string) {
 
 func download(url string, filePtr *os.File, from int, length int) error {
 	fmt.Println("Getting range: ", from, " - ", from+length)
-  chunkSize := (1024 * 1024)
+	chunkSize := (1024 * 1024)
 	client := &http.Client{}
-	for i:=0; i<length; i+=chunkSize {
+	for i := 0; i < length; i += chunkSize {
 		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
 			return err
 		}
-	  range_header := fmt.Sprintf("bytes=%d-%d", from+i, from+i+chunkSize)
+		upto := from + i + chunkSize
+		if i+chunkSize > length {
+			upto = (from + length)
+			fmt.Printf("%d, %d, %d. upto = %d\n", i, chunkSize, length, upto)
+		}
+		range_header := fmt.Sprintf("bytes=%d-%d", from+i, upto-1)
 		req.Header.Add("Range", range_header)
 		resp, err := client.Do(req)
 		if err != nil {
@@ -46,8 +53,8 @@ func download(url string, filePtr *os.File, from int, length int) error {
 }
 
 func main() {
-	totalRoutines := 4
-	tempFilePrefix := "dl_max"
+	totalRoutines := 6
+	tempFilePrefix := "dlmax."
 	argsWithProg := os.Args
 
 	if len(argsWithProg) < 2 {
@@ -56,6 +63,7 @@ func main() {
 	}
 
 	toDownload := argsWithProg[1]
+	destFileName := filepath.Base(toDownload)
 
 	fmt.Println("Downloading... ", toDownload)
 
@@ -65,6 +73,9 @@ func main() {
 	wg.Add(totalRoutines)
 
 	chunkSize := fileSize / totalRoutines
+	extraChunk := fileSize % totalRoutines
+
+	files := make([]string, totalRoutines)
 
 	for i := 0; i < totalRoutines; i++ {
 		prefix := fmt.Sprintf("%d.%s", i+1, tempFilePrefix)
@@ -74,9 +85,31 @@ func main() {
 			os.Exit(2)
 		}
 		fmt.Println("Downloading to..", file.Name())
-		go download(realUrl, file, (i * chunkSize), chunkSize)
+		if totalRoutines-1 == i {
+			go download(realUrl, file, (i * chunkSize), chunkSize+extraChunk)
+		} else {
+			go download(realUrl, file, (i * chunkSize), chunkSize)
+		}
+		files[i] = file.Name()
 	}
 
 	wg.Wait()
-	fmt.Println("Download complete")
+	fmt.Println("Downloaded all parts")
+
+	out, err := os.Create(destFileName)
+	if err != nil {
+		fmt.Println("Could not open destination file")
+		os.Exit(3)
+	}
+	defer out.Close()
+	for i := 0; i < totalRoutines; i++ {
+		in, err := os.Open(files[i])
+		if err != nil {
+			fmt.Println("Bad shit.")
+		}
+		io.Copy(out, in)
+		in.Close()
+		os.Remove(files[i])
+	}
+	fmt.Println("Download success!")
 }
